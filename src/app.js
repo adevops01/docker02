813d24d9e9d0c859bb19413375a02c67501b7b8b
adevops01 <128698740+adevops01@users.noreply.github.com>
2023-06-29 10:20:07 -0500

    praneeth

--- src/app.js
+++ src/app.js
@@ -1,14 +1,11 @@
-const http = require('http');
+const express = require('express')
+const app = express()
+const port = 3000
 
-const hostname = '127.0.0.1';
-const port = 3000;
+app.get('/praneeth', (req, res) => {
+  res.send("This is praneeth's World!")
+})
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port, () => {
+  console.log(`praneeth app listening on port ${port}`)
+})
