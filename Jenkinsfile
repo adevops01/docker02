4310107ba813520d6486362c4ed3900c4c64f99a
adevops01 <128698740+adevops01@users.noreply.github.com>
2023-06-29 10:38:22 -0500

    for harshita

--- Jenkinsfile
+++ Jenkinsfile
@@ -2,7 +2,7 @@ pipeline {
     agent any
     environment{
         REPO = 'moshab679/docker04'
-        TAG = 'chetna'
+        TAG = 'harshita'
     }
     stages {
         stage('Docker image Build') {
--- src/app.js
+++ src/app.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/chetna', (req, res) => {
-  res.send("Chetna's World!")
+app.get('/harshita', (req, res) => {
+  res.send("Harshita's World!")
 })
 
 app.listen(port, () => {
